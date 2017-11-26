870b51d499b4ebc235e8a80cf896704fc51e34c1
Milosz Dura <miloszdura@gmail.com>
2017-11-26 20:07:52 +0000

    added basic grid layout for the search box

--- MovieExplorer/MainPage.xaml.cs
+++ MovieExplorer/MainPage.xaml.cs
@@ -1,17 +1,25 @@
 ﻿using System.Net.Http;
+using Windows.UI.Xaml;
 using MovieExplorer.Data.Film;
 using MovieExplorer.Services.Film;
+using Newtonsoft.Json;
 
 namespace MovieExplorer
 {
     public sealed partial class MainPage
     {
+        private readonly FilmService _filmService;
+
         public MainPage()
         {
             InitializeComponent();
+            _filmService = new FilmService(new FilmRepository(new HttpClient()));
+        }
 
-            var filmService = new FilmService(new FilmRepository(new HttpClient()));
-            var findFilmByTitleResposne = filmService.FindByTitle("transformers");
+        private void Submit_OnClick(object sender, RoutedEventArgs e)
+        {
+            var findFilmByTitleResponse = _filmService.FindByTitle(Query.Text);
+            MovieData.Text = JsonConvert.SerializeObject(findFilmByTitleResponse.Films);
         }
     }
 }
